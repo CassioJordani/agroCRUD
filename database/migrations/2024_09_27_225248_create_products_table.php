d80a6daf24feb61260f7ea34797fe3bb5dc777d0
CassioJordani <141282305+CassioJordani@users.noreply.github.com>
2024-10-20 18:31:50 -0300

    Update 2024_09_27_225248_create_products_table.php

--- database/migrations/2024_09_27_225248_create_products_table.php
+++ database/migrations/2024_09_27_225248_create_products_table.php
@@ -1,30 +1,26 @@
 <?php
 
 use Illuminate\Database\Migrations\Migration;
+
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
+class CreateProductsTable extends Migration
 {
-    /**
-     * Run the migrations.
-     */
-    public function up(): void
-    {
-        Schema::create('products', function (Blueprint $table) {
-            $table->id(); 
-            $table->string('name');
-            $table->text('description')->nullable();
-            $table->decimal('price',10,2);
-            $table->timestamps();
-        });
-    }
+public function up()
+{
+    Schema::create('products', function (Blueprint $table) {
+    $table->id();
+    $table->string('name');
+    $table->text('description')->nullable();
+    $table->decimal('price', 8, 2);
+    $table->timestamps();
+    });
+}
 
-    /**
-     * Reverse the migrations.
-     */
-    public function down(): void
-    {
-        Schema::dropIfExists('products');
-    }
-};
+public function down()
+{
+    Schema::dropIfExists('products');
+}
+
+}
