63dc582ea65a4ca3836779cabc6dd71b9de7ca8c
CassioJordani <141282305+CassioJordani@users.noreply.github.com>
2024-10-20 18:30:28 -0300

    Update ProductController.php

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -1,43 +1,68 @@
 <?php
+
 namespace App\Http\Controllers;
 
-use App\Models\Product;
 use Illuminate\Http\Request;
+use App\Models\Product;
 
 class ProductController extends Controller
 {
-public function index()
-{
+    public function index()
+    {
+        $products = Product::all();
+        return view('products.index', compact('products'));
+    }
 
-$products = Product::all();
-return view('products.index', compact('products'));
-}
+    public function create()
+    {
+        return view('products.create');
+    }
 
-public function create()
-{
-return view('products.create');
-}
+    public function store(Request $request)
+    {
+        $request->validate([
+            'name' => 'required|string|max:255',
+            'description' => 'required|string',
+            'price' => 'required|numeric',
+        ]);
 
-public function store(Request $request)
-{
-$product = Product::create($request->all());
-return redirect()->route('products.index');
-}
+        $product = new Product();
+        $product->name = $request->input('name');
+        $product->description = $request->input('description');
+        $product->price = $request->input('price');
+        $product->save();
 
-public function edit(Product $product)
-{
-return view('products.edit', compact('product'));
-}
+        return redirect()->route('products.index');
+    }
 
-public function update(Request $request, Product $product)
-{
-$product->update($request->all());
-return redirect()->route('products.index');
-}
+    public function edit($id)
+    {
+        $product = Product::find($id);
+        return view('products.edit', compact('product'));
+    }
 
-public function destroy(Product $product)
-{
-$product->delete();
-return redirect()->route('products.index');
-}
+    public function update(Request $request, $id)
+    {
+        $request->validate([
+            'name' => 'required|string|max:255',
+            'description' => 'required|string',
+            'price' => 'required|numeric',
+        ]);
+
+        $product = Product::find($id);
+        $product->name = $request->input('name');
+        $product->description = $request->input('description');
+        $product->price = $request->input('price');
+        $product->save();
+
+        return redirect()->route('products.index');
+    }
+
+    public function destroy($id)
+    {
+        $product = Product::find($id);
+        $product->delete();
+
+        return redirect()->route('products.index');
+    }
 }
