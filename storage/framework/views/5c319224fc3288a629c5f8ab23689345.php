01e9c344612129db80d6d921db2bef8f72fc3a3f
CassioJordani <engs-cassiojordani@camporeal.edu.br>
2024-10-20 16:13:26 -0300

    first commit

---
+++ storage/framework/views/5c319224fc3288a629c5f8ab23689345.php
@@ -0,0 +1,17 @@
+<!-- resources/views/layouts/app.blade.php -->
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="UTF-8">
+<title><?php echo $__env->yieldContent('title', 'Agricultura'); ?></title>
+</head>
+<body>
+<header>
+<h1>Agricultura</h1>
+</header>
+<main>
+<?php echo $__env->yieldContent('content'); ?>
+</main>
+</body>
+</html><?php /**PATH C:\Users\Alunos\Desktop\app\example-app\resources\views/layout/app.blade.php ENDPATH**/ ?>
