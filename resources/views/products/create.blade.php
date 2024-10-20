0cb57390ca2aed86858f46a2f73e280f36cf7a41
CassioJordani <141282305+CassioJordani@users.noreply.github.com>
2024-10-20 18:33:06 -0300

    Update create.blade.php

--- resources/views/products/create.blade.php
+++ resources/views/products/create.blade.php
@@ -1,21 +1,28 @@
 <!-- resources/views/products/create.blade.php -->
-
-@extends('layout.app')
+@extends('layouts.app')
 
 @section('content')
-<h1>Criar Produto</h1>
-<form action="{{ route('products.store') }}" method="post">
-@csrf
-<label for="name">Nome:</label>
-<input type="text" name="name" id="name" required>
-<br>
-<label for="description">Descrição:</label>
-<textarea name="description" id="description"></textarea>
-<br>
-<label for="price">Preço:</label>
-
-<input type="number" step="0.01" name="price" id="price" required>
-<br>
-<button type="submit">Salvar</button>
-</form>
+<div class="container">
+    <h1>Criar Produto</h1>
+    <form action="{{ route('products.store') }}" method="post">
+        @csrf
+        <div class="mb-3">
+            <label for="name" class="form-label">Nome</label>
+            <br>
+            <input type="text" class="form-control" id="name" name="name" required>
+        </div>
+        <div class="mb-3">
+            <label for="description" class="form-label">Descrição</label>
+            <br>
+            <textarea class="form-control" id="description" name="description" required></textarea>
+        </div>
+        <div class="mb-3">
+            <label for="price" class="form-label">Preço</label>
+            <br>
+            <input type="number" class="form-control" id="price" name="price" required>
+        </div>
+        <br>
+        <button type="submit" class="btn btn-primary">Criar Produto</button>
+    </form>
+</div>
 @endsection
