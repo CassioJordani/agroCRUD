5b212a1eb4448a74fd6abcb50d5a7b25d70fb72e
CassioJordani <141282305+CassioJordani@users.noreply.github.com>
2024-10-20 16:37:07 -0300

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -1,13 +1,13 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+
 use App\Http\Controllers\ProductController;
 
-
-Route::get('/products', [ProductController::class, 'index'])->name('products.index');
-Route::post('/products', [ProductController::class, 'store'])->name('products.store');
-Route::get('/products/{id}/edit', [ProdutoController::class, 'edit'])->where('id', '[0-9]+');
+Route::resource('products', ProductController::class);
+Route::get('products/create',[ProductController::class, 'create'])->name('products.create');
+Route::post('products',[ProductController::class, 'store'])->name('products.store');
+Route::get('/products/{id}/edit', [ProductController::class, 'edit'])->where('id', '[0-9]+')->name('products.edit');
 Route::put('/products/{id}', [ProductController::class, 'update'])->where('id', '[0-9]+');
 Route::patch('/products/{id}', [ProductController::class, 'update'])->where('id', '[0-9]+');
-Route::delete('/products/{id}', [ProductController::class, 'destroy'])->where('id', '[0-9]+');
-Route::get('/products/create', [ProductController::class, 'create']);
+Route::delete('/products/{id}', [ProductController::class, 'destroy'])->where('id', '[0-9]+')->name('products.delete');
