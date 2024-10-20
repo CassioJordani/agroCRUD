f4fb024722d0d3b5294f9d5a142cc8502b7abbba
CassioJordani <141282305+CassioJordani@users.noreply.github.com>
2024-10-20 18:32:44 -0300

    Update app.blade.php

--- resources/views/layout/app.blade.php
+++ resources/views/layout/app.blade.php
@@ -3,15 +3,15 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-<meta charset="UTF-8">
-<title>@yield('title', 'Agricultura')</title>
+    <meta charset="UTF-8">
+    <title>@yield('title', 'Agricultura')</title>
 </head>
 <body>
-<header>
-<h1>Agricultura</h1>
-</header>
-<main>
-@yield('content')
-</main>
+    <header>
+        <h1>Agricultura</h1>
+    </header>
+    <main>
+        @yield('content')
+    </main>
 </body>
 </html>
